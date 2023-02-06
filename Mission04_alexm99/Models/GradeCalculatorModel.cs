669d7291fb48f461de3a1fe3170e49864621054d
Alex Merrifield <alex.merrifield13@gmail.com>
2023-02-06 10:37:55 -0800

    MVC Creation

---
+++ Mission04_alexm99/Models/GradeCalculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_alexm99.Models
+{
+    public class GradeCalculatorModel
+    {
+        //This will build the entire get/set for you
+        [Range (0,100)]
+        public string Assignments { get; set; }
+        [Range(0, 100)]
+        public string Group_Project { get; set; }
+        [Range(0, 100)]
+        public string Quizzes { get; set; }
+        [Range(0, 100)]
+        public string Midterm_Exam { get; set; }
+        [Range(0, 100)]
+        public string Final_Exam { get; set; }
+        [Range(0, 100)]
+        public string INTEX { get; set; }
+
+    }
+}
